80980dc68aa6558fffd7042828adecf1b5392989
kaddystrophe <katja.moenius@gmx.de>
2022-07-09 17:12:43 +0200

    NextPlayer Methode in PlayerManager

--- DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/Player/PlayerManager.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/Player/PlayerManager.cs
@@ -23,4 +23,9 @@ public class PlayerManager : MonoBehaviour
             return pinkPlayer;
         return bluePlayer;
     }
+
+    public void NextPlayer() 
+    {
+        currentPlayer = GetOtherPlayer(currentPlayer);
+    }
 }
