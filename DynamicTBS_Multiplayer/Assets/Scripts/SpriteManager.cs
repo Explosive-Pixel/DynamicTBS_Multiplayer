304ccb33e460d89012e42ca5cfb72722b404518a
Ralph Mönius <ralph.moenius@gmx.de>
2022-07-09 15:22:20 +0200

    Sprites hinzugefügt. Sprite-Manager angepasst. Art-Ordnerstruktur angepasst.

--- DynamicTBS_Multiplayer/Assets/Scripts/SpriteManager.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/SpriteManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 public class SpriteManager
 {
@@ -25,9 +22,59 @@ public class SpriteManager
     public static Sprite START_TILE_SPRITE;
     public static Sprite MASTER_START_TILE_SPRITE;
     public static Sprite GOAL_TILE_SPRITE;
+    
+    // UI
+    public static Sprite ABILITY_CIRCLE_SPRITE;
+    public static Sprite ATTACK_CIRCLE_SPRITE;
+    public static Sprite MOVE_CIRCLE_SPRITE;
+    public static Sprite ATTACK_ROW_DOWN_SPRITE;
+    public static Sprite ATTACK_ROW_LEFT_SPRITE;
+    public static Sprite ATTACK_ROW_RIGHT_SPRITE;
+    public static Sprite ATTACK_ROW_UP_SPRITE;
+    public static Sprite COOLDOWN_1_SPRITE;
+    public static Sprite COOLDOWN_2_SPRITE;
+    public static Sprite COOLDOWN_3_SPRITE;
+    public static Sprite HP_1_SPRITE;
+    public static Sprite HP_2_SPRITE;
+    public static Sprite HP_3_SPRITE;
+    public static Sprite HP_4_SPRITE;
+    public static Sprite TANK_BLOCK_FRAME_SPRITE;
 
     public static void LoadSprites()
     {
-        BLUE_MASTER_SPRITE = Resources.Load<Sprite>("");
+        BLUE_MASTER_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Master");
+        PINK_MASTER_SPRITE = Resources.Load<Sprite>("CharacterSprites/Pink_Master");
+        BLUE_TANK_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Tank");
+        PINK_TANK_SPRITE = Resources.Load<Sprite>("CharacterSprites/Pink_Tank");
+        BLUE_SHOOTER_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Shooter");
+        PINK_SHOOTER_SPRITE = Resources.Load<Sprite>("CharacterSprites/Pink_Shooter");
+        BLUE_RUNNER_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Runner");
+        PINK_RUNNER_SPRITE = Resources.Load<Sprite>("CharacterSprites/Pink_Runner");
+        BLUE_MECHANIC_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Mechanic");
+        PINK_MECHANIC_SPRITE = Resources.Load<Sprite>("CharacterSprites/Pink_Mechanic");
+        BLUE_MEDIC_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Medic");
+        PINK_MEDIC_SPRITE = Resources.Load<Sprite>("CharacterSprites/Blue_Medic");
+
+        EMPTY_TILE_SPRITE = Resources.Load<Sprite>("TileSprites/EmptyTile");
+        FLOOR_TILE_SPRITE = Resources.Load<Sprite>("TileSprites/FloorTile");
+        START_TILE_SPRITE = Resources.Load<Sprite>("TileSprites/StartTile");
+        MASTER_START_TILE_SPRITE = Resources.Load<Sprite>("TileSprites/MasterStartTile");
+        GOAL_TILE_SPRITE = Resources.Load<Sprite>("TileSprites/GoalTile");
+        
+        ABILITY_CIRCLE_SPRITE = Resources.Load<Sprite>("UI/AbilityCircle");
+        ATTACK_CIRCLE_SPRITE = Resources.Load<Sprite>("UI/AttackCircle");
+        MOVE_CIRCLE_SPRITE = Resources.Load<Sprite>("UI/MoveCircle");
+        ATTACK_ROW_DOWN_SPRITE = Resources.Load<Sprite>("UI/AttackRowDown");
+        ATTACK_ROW_LEFT_SPRITE = Resources.Load<Sprite>("UI/AttackRowLeft");
+        ATTACK_ROW_RIGHT_SPRITE = Resources.Load<Sprite>("UI/AttackRowRight");
+        ATTACK_ROW_UP_SPRITE = Resources.Load<Sprite>("UI/AttackRowUp");
+        COOLDOWN_1_SPRITE = Resources.Load<Sprite>("UI/Cooldown_1");
+        COOLDOWN_2_SPRITE = Resources.Load<Sprite>("UI/Cooldown_2");
+        COOLDOWN_3_SPRITE = Resources.Load<Sprite>("UI/Cooldown_3");
+        HP_1_SPRITE = Resources.Load<Sprite>("UI/HP_1");
+        HP_2_SPRITE = Resources.Load<Sprite>("UI/HP_2");
+        HP_3_SPRITE = Resources.Load<Sprite>("UI/HP_3");
+        HP_4_SPRITE = Resources.Load<Sprite>("UI/HP_4");
+        TANK_BLOCK_FRAME_SPRITE = Resources.Load<Sprite>("UI/Tank_BlockFrame");
     }
 }
