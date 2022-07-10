1f10ed2fe81597925fa290de4fd6802671a855b4
Ralph Mönius <ralph.moenius@gmx.de>
2022-07-10 14:38:59 +0200

    Standard Draft Button und Skript hinzugefügt.

---
+++ DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/StandardDraft.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardDraft : MonoBehaviour
+{
+    public void PerformStandardDraft()
+    {
+        
+    }
+}
