4234ff5b2a0cd2e5396545ac65e9a6df7bc8aa44
Ralph Mönius <ralph.moenius@gmx.de>
2022-07-10 14:26:01 +0200

    Menüs und Buttons angelegt und mit Funktionalität versehen.

--- DynamicTBS_Multiplayer/Assets/Scripts/GameManager.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject onlineMenuCanvas;
     [SerializeField] private GameObject draftCanvas;
     [SerializeField] private GameObject gameplayCanvas;
+    [SerializeField] private GameObject tutorialCanvas;
+    [SerializeField] private GameObject creditsCanvas;
 
     private List<GameObject> canvasList = new List<GameObject>();
 
@@ -18,62 +20,72 @@ public class GameManager : MonoBehaviour
         SpriteManager.LoadSprites();
         hasGameStarted = false;
         SubscribeEvents();
+        
+        SetCanvasList();
+        HandleMenus(startMenuCanvas);
     }
 
-    
+    private void SetCanvasList()
+    {
+        canvasList.Add(startMenuCanvas);
+        canvasList.Add(onlineMenuCanvas);
+        canvasList.Add(draftCanvas);
+        canvasList.Add(gameplayCanvas);
+        canvasList.Add(tutorialCanvas);
+        canvasList.Add(creditsCanvas);
+    }
     
     #region MenusRegion
 
     public void GoToStartMenu()
     {
-        startMenuCanvas.SetActive(true);
-        onlineMenuCanvas.SetActive(false);
-        draftCanvas.SetActive(false);
-        gameplayCanvas.SetActive(false);
+        HandleMenus(startMenuCanvas);
     }
 
     public void GoToLocalGame()
     {
-        
+        HandleMenus(draftCanvas);
     }
 
     public void GoToOnlineMenu()
     {
-        startMenuCanvas.SetActive(false);
-        onlineMenuCanvas.SetActive(true);
-        draftCanvas.SetActive(false);
-        gameplayCanvas.SetActive(false);
+        HandleMenus(onlineMenuCanvas);
     }
 
     public void GotToDraftScreen()
     {
-        startMenuCanvas.SetActive(false);
-        onlineMenuCanvas.SetActive(false);
-        draftCanvas.SetActive(true);
-        gameplayCanvas.SetActive(false);
+        HandleMenus(draftCanvas);
     }
 
     private void GoToGameplayScreen()
     {
-        startMenuCanvas.SetActive(false);
-        onlineMenuCanvas.SetActive(false);
-        draftCanvas.SetActive(false);
-        gameplayCanvas.SetActive(true);
+        HandleMenus(gameplayCanvas);
     }
 
     public void GoToTutorialScreen()
     {
-        
+        HandleMenus(tutorialCanvas);
     }
 
-    public void GoToCreditScreen()
+    public void GoToCreditsScreen()
     {
-        
+        HandleMenus(creditsCanvas);
     }
 
     private void HandleMenus(GameObject menuCanvas)
     {
-        
+        foreach (GameObject gameObject in canvasList)
+        {
+            if (gameObject == menuCanvas)
+                menuCanvas.SetActive(true);
+            else
+                gameObject.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     #endregion
