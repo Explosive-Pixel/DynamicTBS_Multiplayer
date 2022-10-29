4cec09a805bc1f9976e7e315aa3a8b8543cd0ec2
Ralph Mönius <ralph.moenius@uni-bayreuth.de>
2022-10-29 14:07:05 +0200

    UI Messages for Draft and Placement

--- DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/PlacementEvents.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/PlacementEvents.cs
@@ -8,8 +8,10 @@ public static class PlacementEvents
     public static event CharacterPlacement OnCharacterSelectionForPlacement;
 
     public delegate void PlacementOrder();
-
     public static event PlacementOrder OnAdvancePlacementOrder;
+
+    public delegate void PlacementMessageText();
+    public static event PlacementMessageText OnPlacementMessageChange;
 
     public static void SelectCharacterForPlacement(Character character)
     {
@@ -21,5 +23,11 @@ public static class PlacementEvents
     {
         if (OnAdvancePlacementOrder != null)
             OnAdvancePlacementOrder();
+    }
+
+    public static void ChangePlacementMessage()
+    {
+        if (OnPlacementMessageChange != null)
+            OnPlacementMessageChange();
     }
 }
---
+++ DynamicTBS_Multiplayer/Assets/Scripts/UI/UIDraftMessages.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDraftMessages : MonoBehaviour
+{
+    [SerializeField] private Text draftMessageText;
+    private int messageCounter;
+
+    private void Awake()
+    {
+        messageCounter = 0;
+        // Event subscription
+        DisplayDraftMessages();
+    }
+
+    private void DisplayDraftMessages()
+    {
+        if (messageCounter == 0)
+            draftMessageText.text = "Pink selects 3 units.";
+        if (messageCounter == 1)
+            draftMessageText.text = "Blue selects 3 units.";
+        if (messageCounter == 2)
+            draftMessageText.text = "Pink selects 1 unit.";
+        if (messageCounter == 3)
+            draftMessageText.text = "Blue selects 2 units.";
+        if (messageCounter == 4)
+            draftMessageText.text = "Pink selects 2 units.";
+        if (messageCounter == 5)
+            draftMessageText.text = "Blue selects 2 units.";
+        if (messageCounter == 6)
+            draftMessageText.text = "Pink selects 1 unit.";
+
+        messageCounter += 1;
+    }
+
+    private void OnDestroy()
+    {
+        // Event unsubscription
+    }
+}
