827d218ffc4b93174bdbc94e597e9b8f7cb13705
Ralph Mönius <ralph.moenius@gmx.de>
2022-12-20 13:30:57 +0100

    RecordableStats fertig bis auf online-Speicherung

--- DynamicTBS_Multiplayer/Assets/Scripts/RecordingData/StatsRecorder.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/RecordingData/StatsRecorder.cs
@@ -17,11 +17,14 @@ public class StatsRecorder : MonoBehaviour
     private string runnerShorthand = "RU";
     private string mechanicShorthand = "MC";
     private string medicShorthand = "MD";
+
+    private string blueDraftString;
+    private string pinkDraftString;
     
     private void Awake()
     {
         SubscribeEvents();
-        filePath = Application.dataPath + "/Resources/GameRecords/SaveStats.txt";
+        filePath = Application.dataPath + "/Resources/GameRecords/SaveStats.txt"; //TODO: Saving to a remote file online
     }
     
     // Load old save stats from file and store in variables.
@@ -50,16 +53,96 @@ public class StatsRecorder : MonoBehaviour
         recordableStats.gameNumber += 1;
     }
 
-    private void IncreaseBlueWins(PlayerType winningPlayer)
+    private void RedordWin(PlayerType winningPlayer)
     {
         if (winningPlayer == PlayerType.blue)
+        {
+            // Records the blue win in general
             recordableStats.blueWins += 1;
+            
+            // Records the blue win for the unique draft played
+            foreach (string uniqueDraft in recordableStats.uniqueDraftsDictionary.Keys)
+            {
+                if (uniqueDraft == blueDraftString)
+                {
+                    int gamesPlayedWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item1;
+                    int gamesWonWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item2;
+
+                    gamesPlayedWithThisDraft += 1;
+                    gamesWonWithThisDraft += 1;
+
+                    recordableStats.uniqueDraftsDictionary[uniqueDraft] = (gamesPlayedWithThisDraft, gamesWonWithThisDraft);
+                }
+                else
+                {
+                    recordableStats.uniqueDraftsDictionary.Add(blueDraftString, (1, 1));
+                }
+            }
+
+            // Records the pink loss in the unique draft played
+            foreach (string uniqueDraft in recordableStats.uniqueDraftsDictionary.Keys)
+            {
+                if (uniqueDraft == pinkDraftString)
+                {
+                    int gamesPlayedWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item1;
+                    int gameWonWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item2;
+
+                    gamesPlayedWithThisDraft += 1;
+
+                    recordableStats.uniqueDraftsDictionary[uniqueDraft] = (gamesPlayedWithThisDraft, gameWonWithThisDraft);
+                }
+                else
+                {
+                    recordableStats.uniqueDraftsDictionary.Add(blueDraftString, (1, 0));
+                }
+            }
+        }
+        else
+        {
+            // Records the blue loss for the unique draft played
+            foreach (string uniqueDraft in recordableStats.uniqueDraftsDictionary.Keys)
+            {
+                if (uniqueDraft == blueDraftString)
+                {
+                    int gamesPlayedWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item1;
+                    int gamesWonWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item2;
+
+                    gamesPlayedWithThisDraft += 1;
+
+                    recordableStats.uniqueDraftsDictionary[uniqueDraft] = (gamesPlayedWithThisDraft, gamesWonWithThisDraft);
+                }
+                else
+                {
+                    recordableStats.uniqueDraftsDictionary.Add(blueDraftString, (1, 0));
+                }
+            }
+
+            // Records the pink win in the unique draft played
+            foreach (string uniqueDraft in recordableStats.uniqueDraftsDictionary.Keys)
+            {
+                if (uniqueDraft == pinkDraftString)
+                {
+                    int gamesPlayedWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item1;
+                    int gamesWonWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item2;
+
+                    gamesPlayedWithThisDraft += 1;
+                    gamesWonWithThisDraft += 1;
+
+                    recordableStats.uniqueDraftsDictionary[uniqueDraft] = (gamesPlayedWithThisDraft, gamesWonWithThisDraft);
+                }
+                else
+                {
+                    recordableStats.uniqueDraftsDictionary.Add(blueDraftString, (1, 1));
+                }
+            }
+        }
+        
         SaveStats();
     }
 
     private void IncreaseUnitInDraftCount(List<Character> draftList)
     {
-        // Count how many of each character type there are in the draft of each team
+        // Counts how many of each character type there are in the draft of each team
         int blueTankCount = 0;
         int blueShooterCount = 0;
         int blueRunnerCount = 0;
@@ -75,6 +158,7 @@ public class StatsRecorder : MonoBehaviour
         List<Character> blueDraftList = new List<Character>();
         List<Character> pinkDraftList = new List<Character>();
 
+        // Divides the draft list in two lists for both teams
         foreach (Character character in draftList)
         {
             if (character.GetSide().GetPlayerType() == PlayerType.blue)
@@ -87,6 +171,7 @@ public class StatsRecorder : MonoBehaviour
             }
         }
 
+        // Increases the count of each character by one for each character in the list
         foreach (Character character in blueDraftList)
         {
             if (character.GetCharacterType() == CharacterType.TankChar)
@@ -115,7 +200,7 @@ public class StatsRecorder : MonoBehaviour
                 pinkMedicCount += 1;
         }
 
-        // Put in for loop after rewriting code above as dictionary -> HOW?
+        // Records the units in draft stats in the dictionaries in the RecordableStats class
         if (blueTankCount > 0)
             recordableStats.unitsInDraftDictionary[(blueTankCount, CharacterType.TankChar.ToString())] += 1;
         if (blueShooterCount > 0)
@@ -140,16 +225,13 @@ public class StatsRecorder : MonoBehaviour
 
     private void IncreaseUniqueDraftCombinationCount(List<Character> draftList)
     {
-        // Translates the draft list to a string of shorthands for characters
         List<string> blueShortHandList = new List<string>();
         List<string> pinkShortHandList = new List<string>();
 
         List<Character> blueDraftList = new List<Character>();
         List<Character> pinkDraftList = new List<Character>();
 
-        string blueDraftString;
-        string pinkDraftString;
-
+        // Divides draft list in two lists for each side
         foreach (Character character in draftList)
         {
             if (character.GetSide().GetPlayerType() == PlayerType.blue)
@@ -162,6 +244,7 @@ public class StatsRecorder : MonoBehaviour
             }
         }
 
+        // Translates each side into shorthand string lists
         foreach (Character character in blueDraftList)
         {
             if (character.GetCharacterType() == CharacterType.TankChar)
@@ -190,47 +273,12 @@ public class StatsRecorder : MonoBehaviour
                 pinkShortHandList.Add(medicShorthand);
         }
 
+        // Translates the shorthand lists as strings
         if (blueShortHandList.Count == 7)
         {
             blueDraftString = blueShortHandList[0] + ":" + blueShortHandList[1] + ":" + blueShortHandList[2] + ":" + blueShortHandList[3] + ":" + blueShortHandList[4] + ":" + blueShortHandList[5] + ":" + blueShortHandList[6];
             pinkDraftString = pinkShortHandList[0] + ":" + pinkShortHandList[1] + ":" + pinkShortHandList[2] + ":" + pinkShortHandList[3] + ":" + pinkShortHandList[4] + ":" + pinkShortHandList[5] + ":" + pinkShortHandList[6];
-
-            foreach (string uniqueDraft in recordableStats.uniqueDraftsDictionary.Keys)
-            {
-                if (uniqueDraft == blueDraftString)
-                {
-                    int gamesPlayedWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item1;
-                    int gameWonWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item2;
-
-                    gamesPlayedWithThisDraft += 1;
-
-                    recordableStats.uniqueDraftsDictionary[uniqueDraft] = (gamesPlayedWithThisDraft, gameWonWithThisDraft);
-                }
-                else
-                {
-                    recordableStats.uniqueDraftsDictionary.Add(blueDraftString, (1, 0));
-                }
-            }
-
-            foreach (string uniqueDraft in recordableStats.uniqueDraftsDictionary.Keys)
-            {
-                if (uniqueDraft == pinkDraftString)
-                {
-                    int gamesPlayedWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item1;
-                    int gameWonWithThisDraft = recordableStats.uniqueDraftsDictionary[uniqueDraft].Item2;
-
-                    gamesPlayedWithThisDraft += 1;
-
-                    recordableStats.uniqueDraftsDictionary[uniqueDraft] = (gamesPlayedWithThisDraft, gameWonWithThisDraft);
-                }
-                else
-                {
-                    recordableStats.uniqueDraftsDictionary.Add(blueDraftString, (1, 0));
-                }
-            }
         }
-
-        // TODO: After game ends record the win stat of the draft that won
     }
 
     #region EventsRegion
@@ -241,7 +289,7 @@ public class StatsRecorder : MonoBehaviour
         GameManager.OnStartRecording += IncreaseGameNumber;
         DraftEvents.OnDeliverCharacterList += IncreaseUnitInDraftCount;
         DraftEvents.OnDeliverCharacterList += IncreaseUniqueDraftCombinationCount;
-        GameplayEvents.OnGameOver += IncreaseBlueWins;
+        GameplayEvents.OnGameOver += RedordWin;
     }
 
     private void UnsubscribeEvents()
@@ -250,7 +298,7 @@ public class StatsRecorder : MonoBehaviour
         GameManager.OnStartRecording -= IncreaseGameNumber;
         DraftEvents.OnDeliverCharacterList -= IncreaseUnitInDraftCount;
         DraftEvents.OnDeliverCharacterList -= IncreaseUniqueDraftCombinationCount;
-        GameplayEvents.OnGameOver -= IncreaseBlueWins;
+        GameplayEvents.OnGameOver -= RedordWin;
     }
 
     private void OnDestroy()
