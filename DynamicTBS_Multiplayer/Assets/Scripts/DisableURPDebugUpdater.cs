1d2ac46d671c468632b3e438f1cb8665b558a16b
kaddystrophe <katja.moenius@gmx.de>
2022-12-27 19:00:53 +0100

    Fix Console errors on build startup

---
+++ DynamicTBS_Multiplayer/Assets/Scripts/DisableURPDebugUpdater.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DisableURPDebugUpdater : MonoBehaviour
+{
+    private void Awake()
+    {
+        DebugManager.instance.enableRuntimeUI = false;
+
+        // Debug.LogError(null); // Uncomment this to show console on startup
+    }
+}
+
