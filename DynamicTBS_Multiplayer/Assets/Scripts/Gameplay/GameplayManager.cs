435973df210b7809e623614e58c4ad8ceeb81cde
Ralph Mönius <ralph.moenius@uni-bayreuth.de>
2022-12-28 17:43:56 +0100

    Update GameplayManager.cs
    Win/loss/draw conditions outline.

--- DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/GameplayManager.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/GameplayManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    // TODO: Implement the following win/loss/draw-conditions...
+    // WIN/LOSS: A side wins, if...
+    //           - the master unit is first to activate its active ability on the goal square.
+    //           - the opposing master unit is killed while the own master unit is still alive.
+    //           - the opposing side has no more legal moves, but 2 actions in their turn.
+    // DRAW: A game is drawn, if...
+    //           - no mechanic units are alive on either side...
+    //           & the goal square can't be reached...
+    //           & both master units can't be killed.
+
     #region Gameplay Config
 
     private const int maxActionsPerRound = 2;
