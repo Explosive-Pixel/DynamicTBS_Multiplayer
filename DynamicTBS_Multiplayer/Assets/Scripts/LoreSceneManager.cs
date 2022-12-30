9e3db04e2997fcdedacd9043aacf262b0da1b0b0
Ralph Mönius <ralph.moenius@uni-bayreuth.de>
2022-12-30 10:34:26 +0100

    Lore scene added; script for navigating the lore scene added

---
+++ DynamicTBS_Multiplayer/Assets/Scripts/LoreSceneManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoreSceneManager : MonoBehaviour
+{
+    // TODO: Logic for switching canvases in the lore scene and to return to the main menu.
+}
