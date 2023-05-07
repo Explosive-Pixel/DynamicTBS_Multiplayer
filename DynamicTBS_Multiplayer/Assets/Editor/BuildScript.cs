ab7463c78b10392549c945afcad8fa3958d22aa7
kaddystrophe <katja.moenius@gmx.de>
2023-05-07 14:34:50 +0200

    Easy builds over build menu

--- DynamicTBS_Multiplayer/Assets/Editor/BuildScript.cs
+++ DynamicTBS_Multiplayer/Assets/Editor/BuildScript.cs
@@ -12,20 +12,17 @@ public static class BuildScript
         Server
     }
 
+    public enum Platform
+    {
+        Windows,
+        Mac,
+        Linux
+    }
+
     static string[] scenePaths = { "Assets/Scenes/Current/00_ServerScene.unity", "Assets/Scenes/Current/01_MainMenuScene.unity", "Assets/Scenes/Current/02_OnlineMenuScene.unity", "Assets/Scenes/Current/03_GameScene.unity", "Assets/Scenes/Current/04_TutorialScene.unity", "Assets/Scenes/Current/05_LoreScene.unity", "Assets/Scenes/Current/06_CreditsScene.unity" };
 
     public static string[] ConfigureScenes(BuildType buildType)
     {
-        /* EditorBuildSettings.scenes = new EditorBuildSettingsScene[scenePaths.Length];
-
-         EditorBuildSettings.scenes[0] = new EditorBuildSettingsScene(scenePaths[0], buildType == BuildType.Server);
-
-         for (int i = 1; i < scenePaths.Length; i++)
-         {
-             EditorBuildSettings.scenes[i] = new EditorBuildSettingsScene(scenePaths[i], buildType == BuildType.Client);
-         } */
-
-
         var scenes = new List<string>();
         if (buildType == BuildType.Server)
         {
@@ -41,45 +38,84 @@ public static class BuildScript
         return scenes.ToArray();
     }
 
-    [MenuItem("Build/Server Build")]
-    public static void PerformServerBuild()
+    [MenuItem("Build/Server Build (Windows)")]
+    public static void PerformServerBuildWindows()
     {
-        PerformBuild(BuildType.Server);
+        PerformBuild(BuildType.Server, Platform.Windows);
     }
 
-    [MenuItem("Build/Client Build")]
-    public static void PerformClientBuild()
+    [MenuItem("Build/Client Build (Windows)")]
+    public static void PerformClientBuildWindows()
     {
-        PerformBuild(BuildType.Client);
+        PerformBuild(BuildType.Client, Platform.Windows);
     }
 
-    private static void PerformBuild(BuildType buildType)
+    [MenuItem("Build/All Builds (Windows)")]
+    public static void PerformAllBuildsWindows()
+    {
+        PerformServerBuildWindows();
+        PerformClientBuildWindows();
+    }
+
+    [MenuItem("Build/Server Build (Mac iOS)")]
+    public static void PerformServerBuildMac()
+    {
+        PerformBuild(BuildType.Server, Platform.Mac);
+    }
+
+    [MenuItem("Build/Client Build (Mac iOS)")]
+    public static void PerformClientBuildMac()
+    {
+        PerformBuild(BuildType.Client, Platform.Mac);
+    }
+
+    [MenuItem("Build/All Builds (Mac iOS)")]
+    public static void PerformAllBuildsMac()
+    {
+        PerformServerBuildMac();
+        PerformClientBuildMac();
+    }
+
+    [MenuItem("Build/Server Build (Linux)")]
+    public static void PerformServerBuildLinux()
+    {
+        PerformBuild(BuildType.Server, Platform.Linux);
+    }
+
+    [MenuItem("Build/Client Build (Linux)")]
+    public static void PerformClientBuildLinux()
+    {
+        PerformBuild(BuildType.Client, Platform.Linux);
+    }
+
+    [MenuItem("Build/All Builds (Linux)")]
+    public static void PerformAllBuildsLinux()
+    {
+        PerformServerBuildLinux();
+        PerformClientBuildLinux();
+    }
+
+    private static void PerformBuild(BuildType buildType, Platform platform)
     {
         Debug.Log("Performing build: " + buildType);
-        string buildPath = "Builds/" + buildType.ToString() +"/Skyrats.exe";
+        string buildPath = "Builds/" + platform.ToString() + "/" + buildType.ToString() +"/Skyrats.exe";
 
-        // Get the build settings
-       /* BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        BuildTarget buildTarget = BuildTarget.StandaloneWindows64;
+        if (platform == Platform.Mac)
+            buildTarget = BuildTarget.iOS;
+        else if (platform == Platform.Linux)
+            buildTarget = BuildTarget.StandaloneLinux64;
 
-        buildPlayerOptions.scenes = ConfigureScenes(buildType);
-
-        buildPlayerOptions.locationPathName = buildPath;
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
-        buildPlayerOptions.options = BuildOptions.None;
-
-        // Build the player
-        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);*/
-
-        BuildPipeline.BuildPlayer(ConfigureScenes(buildType), buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        BuildReport report = BuildPipeline.BuildPlayer(ConfigureScenes(buildType), buildPath, buildTarget, BuildOptions.None);
 
         // Check if the build succeeded
-       /* if (report.summary.result == BuildResult.Succeeded)
+        if (report.summary.result == BuildResult.Succeeded)
         {
-            Debug.Log("Build succeeded!");
+            Debug.Log(buildType + " Build succeeded for platform " + platform + "!");
         }
         else
         {
-            Debug.LogError("Build failed!");
-        }*/
+            Debug.LogError(buildType + " Build for platform " + platform + "failed.");
+        }
     }
 }
