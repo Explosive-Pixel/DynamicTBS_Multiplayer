6ab6292eb698dbdd7bd8de622b64aaa6777a4a70
Katja Mönius <katja.moenius@gmx.de>
2024-04-27 13:18:18 +0200

    Add new Captain AA Charm

---
+++ DynamicTBS_Multiplayer/Assets/Scripts/Gameplay/Character/CharacterStates/CharmedState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharmedState : MonoBehaviour, IState
+{
+    private Character character;
+
+    public static CharmedState Create(GameObject parent, int charmDuration)
+    {
+        CharmedState cs = parent.AddComponent<CharmedState>();
+        cs.Init(charmDuration);
+        return cs;
+    }
+
+    private void Init(int charmDuration)
+    {
+        character = gameObject.GetComponent<Character>();
+        SwapSide();
+
+        RoundBasedCounter.Create(gameObject, charmDuration, Destroy);
+    }
+
+    private void SwapSide()
+    {
+        character.Side = PlayerManager.GetOtherSide(character.Side);
+    }
+
+    public void Destroy()
+    {
+        Destroy(this);
+    }
+
+    private void OnDestroy()
+    {
+        SwapSide();
+    }
+}
