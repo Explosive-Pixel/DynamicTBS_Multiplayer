c7ca4da2a62c2f61c2d614d0e76e67dcfded98cf
Katja Mönius <katja.moenius@gmx.de>
2025-05-17 10:49:13 +0200

    Hotfix Move Bug

--- DynamicTBS_Multiplayer/Assets/Scripts/UI/Animations/MoveEffect.cs
+++ DynamicTBS_Multiplayer/Assets/Scripts/UI/Animations/MoveEffect.cs
@@ -27,6 +27,6 @@ public class MoveEffect : IndicatorEffect
 
     private void OnDestroy()
     {
-        GameplayEvents.OnFinishAction += OnMoveCharacter;
+        GameplayEvents.OnFinishAction -= OnMoveCharacter;
     }
 }
