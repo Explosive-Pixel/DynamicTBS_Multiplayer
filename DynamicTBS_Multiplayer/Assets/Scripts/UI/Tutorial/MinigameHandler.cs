2cca1e35df006d2c0d5d63e14ee27897a6a8a5c0
Katja Mönius <katja.moenius@gmx.de>
2026-01-03 11:42:34 +0100

    Start with minigame stuff

---
+++ DynamicTBS_Multiplayer/Assets/Scripts/UI/Tutorial/MinigameHandler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameHandler : MonoBehaviour
+{
+    [SerializeField]
+    private List<TileDefinition> layout;
+}
